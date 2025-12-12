d431d3aebb994812c91688b8f1a4a381f09dcba8
Angel Santamaria <angelsantamaria0412@gmail.com>
2025-12-12 11:35:59 -0500

    section 15

--- 06-reactive-forms-app/src/app/country/pages/country-page/country-page.ts
+++ 06-reactive-forms-app/src/app/country/pages/country-page/country-page.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, effect, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
+import { count, filter, of, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-page.html',
-  styles: ``
+  styles: ``,
 })
 export class CountryPage {
+  fb = inject(FormBuilder);
+  countryService = inject(CountryService);
 
+  regions = signal(this.countryService.regions);
+
+  countriesByRegion = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+
+  onFormChanged = effect((onCleanUp) => {
+    const regionSubscription = this.onRegionChanged();
+    const countrySubscription = this.onCountryChanged();
+
+    onCleanUp(() => {
+      regionSubscription.unsubscribe();
+      countrySubscription.unsubscribe();
+    });
+  });
+
+  onRegionChanged() {
+    return this.myForm
+      .get('region')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => this.myForm.get('border')!.setValue('')),
+        tap(() => {
+          this.borders.set([]);
+          this.countriesByRegion.set([]);
+        }),
+        switchMap((region) => this.countryService.getCountriesByRegion(region ?? ''))
+      )
+      .subscribe((countries) => {
+        this.countriesByRegion.set(countries);
+      });
+  }
+
+  onCountryChanged() {
+    return this.myForm
+      .get('country')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('border')!.setValue('')),
+        tap(() => {
+          this.borders.set([]);
+        }),
+        filter((value) => value!.length > 0),
+        switchMap((alphaCode) => this.countryService.getCountryByAlphaCode(alphaCode ?? '')),
+        switchMap((country) => this.countryService.getCountryNamesByCodeArray(country.borders))
+      )
+      .subscribe((borders) => {
+        this.borders.set(borders);
+      });
+  }
 }
