315d3c835059250385c29214584f1d7e07dab454
Angel Santamaria <angelsantamaria0412@gmail.com>
2025-12-17 17:31:02 -0500

    hashstrategy

--- 07-lifecycle-hooks/src/app/app.config.ts
+++ 07-lifecycle-hooks/src/app/app.config.ts
@@ -1,5 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
 
@@ -8,6 +13,11 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     // Habilitar señales
-    provideZonelessChangeDetection()
-  ]
+    provideZonelessChangeDetection(),
+    // HashStrategy (Para despliegue)
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+  ],
 };
